987bdb7632698472e3c2d17006cc1f44264ce4ca
root <root@sudheer4641.mylabserver.com>
2016-12-29 03:38:28 +0000

    added basic Apache cookboook

---
+++ cookbooks/apache/metadata.rb
@@ -0,0 +1,7 @@
+name 'apache'
+maintainer 'Sudheer'
+maintainer_email 'Sudheer.sk3@gmail.com'
+license 'all_rights'
+description 'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version '0.1.0'
---
+++ cookbooks/apache/recipes/websites.rb
@@ -0,0 +1,5 @@
+file 'default www' do
+   path '/var/www/html/inex.html'
+   content 'Helllo World!'
+   
+end
